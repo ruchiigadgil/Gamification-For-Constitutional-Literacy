b1ef8fc5d4e2ffd1ed5ec478dc7f7407db8c7ae4
rohana-2005 <rohanamahimkar28@gmail.com>
2025-11-22 13:10:32 +0530

    Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5002/api/auth/login", {
+      const response = await fetch("http://localhost:3001/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
